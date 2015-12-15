b2a6c0a3171c2de73fd60bb9bd9f541e9e3d726e
Huynh Nguyen <huynhdev@live.com>
2015-12-15 05:24:10 -0500

    Added hero2, enemy1, enemy2

---
+++ RuinaDefense_2/Enemy1.swift
@@ -0,0 +1,80 @@
+// ---------------------------------------
+// Sprite definitions for 'Enemy1'
+// Generated with TexturePacker 4.0.1
+//
+// http://www.codeandweb.com/texturepacker
+// ---------------------------------------
+
+import SpriteKit
+
+
+class Enemy1 {
+
+    // sprite names
+    let ATTACK_1_ = "attack (1)"
+    let ATTACK_2_ = "attack (2)"
+    let ATTACK_3_ = "attack (3)"
+    let ATTACK_4_ = "attack (4)"
+    let ATTACK_5_ = "attack (5)"
+    let ATTACK_6_ = "attack (6)"
+    let ATTACK_7_ = "attack (7)"
+    let ATTACK_8_ = "attack (8)"
+    let DEAD_1_   = "dead (1)"
+    let DEAD_2_   = "dead (2)"
+    let DEAD_3_   = "dead (3)"
+    let DEAD_4_   = "dead (4)"
+    let DEFEND_1_ = "defend (1)"
+    let DEFEND_2_ = "defend (2)"
+    let DEFEND_3_ = "defend (3)"
+    let HURT      = "hurt"
+    let IDLE      = "idle"
+    let WALK_10_  = "walk (10)"
+    let WALK_11_  = "walk (11)"
+    let WALK_12_  = "walk (12)"
+    let WALK_1_   = "walk (1)"
+    let WALK_2_   = "walk (2)"
+    let WALK_3_   = "walk (3)"
+    let WALK_4_   = "walk (4)"
+    let WALK_5_   = "walk (5)"
+    let WALK_6_   = "walk (6)"
+    let WALK_7_   = "walk (7)"
+    let WALK_8_   = "walk (8)"
+    let WALK_9_   = "walk (9)"
+
+
+    // load texture atlas
+    let textureAtlas = SKTextureAtlas(named: "Enemy1")
+
+
+    // individual texture objects
+    func attack_1_() -> SKTexture { return textureAtlas.textureNamed(ATTACK_1_) }
+    func attack_2_() -> SKTexture { return textureAtlas.textureNamed(ATTACK_2_) }
+    func attack_3_() -> SKTexture { return textureAtlas.textureNamed(ATTACK_3_) }
+    func attack_4_() -> SKTexture { return textureAtlas.textureNamed(ATTACK_4_) }
+    func attack_5_() -> SKTexture { return textureAtlas.textureNamed(ATTACK_5_) }
+    func attack_6_() -> SKTexture { return textureAtlas.textureNamed(ATTACK_6_) }
+    func attack_7_() -> SKTexture { return textureAtlas.textureNamed(ATTACK_7_) }
+    func attack_8_() -> SKTexture { return textureAtlas.textureNamed(ATTACK_8_) }
+    func dead_1_() -> SKTexture   { return textureAtlas.textureNamed(DEAD_1_) }
+    func dead_2_() -> SKTexture   { return textureAtlas.textureNamed(DEAD_2_) }
+    func dead_3_() -> SKTexture   { return textureAtlas.textureNamed(DEAD_3_) }
+    func dead_4_() -> SKTexture   { return textureAtlas.textureNamed(DEAD_4_) }
+    func defend_1_() -> SKTexture { return textureAtlas.textureNamed(DEFEND_1_) }
+    func defend_2_() -> SKTexture { return textureAtlas.textureNamed(DEFEND_2_) }
+    func defend_3_() -> SKTexture { return textureAtlas.textureNamed(DEFEND_3_) }
+    func hurt() -> SKTexture      { return textureAtlas.textureNamed(HURT) }
+    func idle() -> SKTexture      { return textureAtlas.textureNamed(IDLE) }
+    func walk_10_() -> SKTexture  { return textureAtlas.textureNamed(WALK_10_) }
+    func walk_11_() -> SKTexture  { return textureAtlas.textureNamed(WALK_11_) }
+    func walk_12_() -> SKTexture  { return textureAtlas.textureNamed(WALK_12_) }
+    func walk_1_() -> SKTexture   { return textureAtlas.textureNamed(WALK_1_) }
+    func walk_2_() -> SKTexture   { return textureAtlas.textureNamed(WALK_2_) }
+    func walk_3_() -> SKTexture   { return textureAtlas.textureNamed(WALK_3_) }
+    func walk_4_() -> SKTexture   { return textureAtlas.textureNamed(WALK_4_) }
+    func walk_5_() -> SKTexture   { return textureAtlas.textureNamed(WALK_5_) }
+    func walk_6_() -> SKTexture   { return textureAtlas.textureNamed(WALK_6_) }
+    func walk_7_() -> SKTexture   { return textureAtlas.textureNamed(WALK_7_) }
+    func walk_8_() -> SKTexture   { return textureAtlas.textureNamed(WALK_8_) }
+    func walk_9_() -> SKTexture   { return textureAtlas.textureNamed(WALK_9_) }
+
+}
