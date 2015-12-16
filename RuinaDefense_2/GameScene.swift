66476225bea941828bb21494dda2462b8185c86e
Huynh Nguyen <huynhdev@live.com>
2015-12-16 06:50:01 -0500

    merge

--- RuinaDefense_2/GameScene.swift
+++ RuinaDefense_2/GameScene.swift
@@ -213,7 +213,7 @@ class GameScene: SKScene {
         //Add to scene
         background.addChild(hero2)
         
-        let unit = Unit(spriteNode: hero2, hp: 5, def: 0, dmg: 1)
+        let unit = Unit(spriteNode: hero2, hp: 5, def: 0, dmg: 2)
         
         //Animates the hero
         let run = SKAction.animateWithTextures(randomHero2Animation(), timePerFrame: 0.1)
@@ -272,7 +272,7 @@ class GameScene: SKScene {
         //Add to scene
         background.addChild(enemy2)
         
-        let unit = Unit(spriteNode: enemy2, hp: 5, def: 0, dmg: 1)
+        let unit = Unit(spriteNode: enemy2, hp: 2, def: 0, dmg: 3)
         
         //Animates the hero
         //let run = SKAction.animateWithTextures(Enemy2_Sheet.run(), timePerFrame: 0.033)
